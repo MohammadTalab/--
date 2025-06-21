7bf91c51756380e10228f98be62ad032a17e710e
MohammadTalab <hadarawerwer@gmail.com>
2025-06-21 23:33:52 +0300

    تعديل

---
+++ product_add.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>إضافة منتج</title>
+    <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon"/>
+    <link rel="stylesheet" href="../style/style.css"/>
+</head>
+<body>
+
+<?php
+include_once('header.php');
+include_once('menu.php');
+require_once('connect.php');
+
+
+$sql = "SELECT * FROM `category` ORDER BY `name`";
+$res_category = mysqli_query($coon, $sql);
+?>
+
+<form action="book_insert.php" method="post" enctype="multipart/form-data">
+    <table class="form">
+        <caption>إضافة منتج</caption>
+
+        <tr>
+            <td><label for="name">اسم المنتج:</label></td>
+            <td><input type="text" name="name" id="name" required></td>
+        </tr>
+
+        <tr>
+            <td><label for="publisher">صاحب المنتج:</label></td>
+            <td><input type="text" name="publisher" id="publisher" required></td>
+        </tr>
+
+        <tr>
+            <td><label for="category">التصنيف:</label></td>
+            <td>
+                <select name="category" id="category" required>
+                    <option value="">اختر التصنيف </option>
+                    <?php
+                    while ($row = mysqli_fetch_assoc($res_category)) {
+                        echo "<option value='{$row['id']}'>{$row['name']}</option>";
+                    }
+                    ?>
+                </select>
+            </td>
+        </tr>
+
+        <tr>
+            <td><label for="description">الوصف:</label></td>
+            <td><textarea name="description" id="description" rows="4" cols="30"></textarea></td>
+        </tr>
+
+        <tr>
+            <td><label for="price">السعر:</label></td>
+            <td><input type="number" name="price" id="price" step="0.01" required></td>
+        </tr>
+
+        <tr>
+            <td><label for="cover">صورة المنتج:</label></td>
+            <td><input type="file" name="cover" id="cover" accept="image/*" required></td>
+        </tr>
+
+        <tr>
+            <td colspan="2" style="text-align:center;">
+                <input type="submit" name="submit" value="إرسال">
+            </td>
+        </tr>
+    </table>
+</form>
+
+<?php include_once('include/footer.php'); ?>
+
+</body>
+</html>
