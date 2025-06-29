ad9f65b6590559d0d88bfef38d9b8fc1f9474248
faeqahaddad <faeqa.haddad.dev@gmail.com>
2025-06-29 16:46:51 +0300

    completed.php

---
+++ completed.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <title>صفحة إتمام الشراء</title>
+</head>
+<body>
+
+  <div class="checkout-container">
+    <h1>إتمام الشراء</h1>
+
+    <div class="order-summary">
+      <h2>ملخص الطلب</h2>
+      <div class="item">
+        <p>اسم المنتج: جهاز لابتوب</p>
+        <p>الكمية: 2</p>
+        <p>السعر: 1000</p>
+      </div>
+      <p class="total">الإجمالي: 2000</p>
+    </div>
+
+    <div class="shipping-info">
+      <h2>معلومات الشحن</h2>
+
+      <p>الاسم الكامل:</p>
+      <input type="text"><br><br>
+
+      <p>رقم الهاتف:</p>
+      <input type="tel"><br><br>
+
+      <p>المدينة:</p>
+      <input type="text"><br><br>
+
+      <p>الشارع / رقم المنزل:</p>
+      <input type="text"><br><br>
+
+      <p>ملاحظات إضافية:</p>
+      <input type="text"><br><br>
+
+    </div>
+
+   <p>طريقة الدفع:</p>
+
+<button>الدفع عند الاستلام</button><br><br>
+<button>بطاقة بنكية</button><br><br>
+
+
+
+
+    <br><br>
+    <button class="button">تأكيد الشراء</button>
+  </div>
+
+</body>
+</html>
