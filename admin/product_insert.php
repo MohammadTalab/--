f739c395f1192ccc08182e7f8b5a5f5f004fb362
faeqahaddad <faeqa.haddad.dev@gmail.com>
2025-07-03 18:12:30 +0300

    تعديل

--- admin/product_insert.php
+++ admin/product_insert.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['role'])) {
     header("Location: admin_login.php");
     exit();
 }
--- admin/product_show.php
+++ admin/product_show.php
@@ -1,3 +1,17 @@
+
+<?php
+session_start();
+
+if (!isset($_SESSION['role'])) {
+    header("Location: admin_login.php");
+    exit();
+}
+
+if ($_SESSION['role'] !== 'admin') {
+    echo "لا تملك صلاحية الوصول لهذه الصفحة.";
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
